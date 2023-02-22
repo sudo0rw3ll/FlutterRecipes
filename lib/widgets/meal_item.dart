77e8d9cd336ae60d016542050d5876ed66f5560b
vnikolic7821rn <vnikolic7821rn@raf.rs>
2023-02-22 16:23:15 +0100

    Added option of removing items from category

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,6 +10,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   const MealItem(
       {required this.id,
@@ -17,7 +18,8 @@ class MealItem extends StatelessWidget {
       required this.imageUrl,
       required this.duration,
       required this.complexity,
-      required this.affordability});
+      required this.affordability,
+      required this.removeItem});
 
   String get complexityText {
     switch (complexity) {
@@ -54,7 +56,13 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(MealDetailScreen.routeName, arguments: id);
+    Navigator.of(ctx)
+        .pushNamed(MealDetailScreen.routeName, arguments: id)
+        .then((value) {
+          if(value != null){
+            removeItem(value);
+          }
+    });
   }
 
   @override
